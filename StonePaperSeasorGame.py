42eb2e655c8ad28be05aa118d4da6ce313b8a4d6
biswajt4472 <113298744+biswajt4472@users.noreply.github.com>
2023-05-23 19:46:31 +0530

    Create StonePaperSeasorGame.py

---
+++ StonePaperSeasorGame.py
@@ -0,0 +1,30 @@
+# A Sample Game For Rock,Paper,Seasor
+import random
+from termcolor import colored
+
+# Welcome to Our Game
+print("Your Choice : ", ("Rock", 'Paper', 'Seasor'), sep='\n')
+choice = ['Rock', 'Paper', 'Seasor']
+x = random.choice(choice)
+
+custom = input()
+custom = custom.capitalize()
+print(("You : ", custom))
+print(("Computer : ", x))
+if custom == x:
+    print("Match Draw")
+elif custom == 'Rock' and x == 'Seasor':
+    print("You Win The Match Vigorously")
+elif custom == 'Rock' and x == 'Paper':
+    print("Sorry You Have Loose The Game....")
+elif custom == 'Paper' and x == 'Rock':
+    print("Unbelievable , You Beat The Computer")
+elif custom == 'Paper' and x == 'Seasor':
+    print("Sorry You Have Loose The Game....")
+elif custom == 'Seasor' and x == 'Paper':
+    print("What A Surprise , You Won That")
+elif custom == 'Seasor' and x == 'Rock':
+    print("It's A Sad Thing ....You Have Loose The Game....")
+else:
+    print(colored("Wrong Input ..... ", 'red'))
+    print("Please Check Your Input Data Matches With The Following One Or Not", ("Rock", 'Paper', 'Seasor'))
