fee6263b5e130d7aee91ddc2c2e258515ca839b4
biswajt4472 <113298744+biswajt4472@users.noreply.github.com>
2023-05-23 20:18:20 +0530

    Update StonePaperSeasorGame.py

--- StonePaperSeasorGame.py
+++ StonePaperSeasorGame.py
@@ -4,8 +4,8 @@ from termcolor import colored
 
 # Welcome to Our Game
 
-print("Your Choice : ", ("Rock", 'Paper', 'Seasor'), sep='\n')
-choice = ['Rock', 'Paper', 'Seasor']
+print("Your Choice : ", ("Rock", 'Paper', 'Scissors'), sep='\n')
+choice = ['Rock', 'Paper', 'Scissors']
 x = random.choice(choice)
 
 custom = input()
@@ -14,17 +14,17 @@ print(("You : ", custom))
 print(("Computer : ", x))
 if custom == x:
     print("Match Draw")
-elif custom == 'Rock' and x == 'Seasor':
+elif custom == 'Rock' and x == 'Scissors':
     print("You Win The Match Vigorously")
 elif custom == 'Rock' and x == 'Paper':
     print("Sorry You Have Loose The Game....")
 elif custom == 'Paper' and x == 'Rock':
     print("Unbelievable , You Beat The Computer")
-elif custom == 'Paper' and x == 'Seasor':
+elif custom == 'Paper' and x == 'Scissors':
     print("Sorry You Have Loose The Game....")
 elif custom == 'Seasor' and x == 'Paper':
     print("What A Surprise , You Won That")
-elif custom == 'Seasor' and x == 'Rock':
+elif custom == 'Scissors' and x == 'Rock':
     print("It's A Sad Thing ....You Have Loose The Game....")
 else:
     print(colored("Wrong Input ..... ", 'red'))
